6a55fa7051fa680d8fc5e5169128b15cfa6de8d7
chenghsienyu <chenghsienyu@aim.com>
2022-11-24 20:53:53 +0100

    update script

--- Assets/Scripts/ViewFollowing.cs
+++ Assets/Scripts/ViewFollowing.cs
@@ -8,17 +8,30 @@ public class ViewFollowing : MonoBehaviour
     public Transform target;
     public float soomthspeed = 0.125f;
     public Vector3 offset;
+    public Vector3 offset_inverted;
+    public bool isFollowing = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - target.position;
+        transform.position = target.transform.position + offset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.transform.position + offset;
+        if (isFollowing)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, soomthspeed);
+            transform.position = smoothedPosition;
+        }else{
+            offset_inverted = new Vector3(transform.position.x * -1.0f - target.position.x, offset.y, offset.z);
+            Vector3 desiredPosition = target.position + offset_inverted;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, soomthspeed);
+            transform.position = smoothedPosition;
+        }
     }
 }
